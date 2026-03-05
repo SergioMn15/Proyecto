2386fa62b1a05377d00a923c7628cdc185462ca0
SergioMancilla <22460303@colima.tecnm.mx>
2026-03-05 11:18:41 -0600

    Mi primer servidor en la nube

---
+++ Backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Conexión a Aiven usando tus datos
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } // Aiven requiere SSL activo
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error de conexión:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos "proyecto" en Aiven');
+});
+
+// Ruta de prueba para el Login
+app.post('/login', (req, res) => {
+    const { telefono, password } = req.body;
+    const query = "SELECT nombre, rol FROM usuarios WHERE telefono = ? AND password = ?";
+
+    db.query(query, [telefono, password], (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (result.length > 0) {
+            res.json({ success: true, usuario: result[0] });
+        } else {
+            res.json({ success: false, message: "Datos incorrectos" });
+        }
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(` Servidor corriendo en http://localhost:${process.env.PORT}`);
+});
